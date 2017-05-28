24fbfdb555757aef8f3c4fed8e58ba041fbd652c
Brandon Caruso <bcaruso@uw.edu>
2017-05-28 16:37:49 -0700

    first version live, not mobile friendly yet

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,5 +1,3 @@
-// URL updates and the element focus is maintained
-// originally found via in Update 3 on http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
 
 $(document).ready(function(){
 // filter handling for a /dir/ OR /indexordefault.page
@@ -11,7 +9,7 @@ function filterPath(string) {
 }
 
 $('a[href*="#"]').each(function () {
-  console.log(this.hash +" " +inView(this.hash))
+  inView(this.hash)
 });
 
 $('#work').scroll(function() {
@@ -28,7 +26,6 @@ function inView(hash){
   var workbottom = worktop + $('#work').outerHeight(true)/4;
 
   if(divtop >= worktop &&  divtop <= workbottom){
-    console.log(target+" in view")
     $('a[href*="'+hash+'"]').addClass("inview");
     $('a[href*="'+hash+'"]').siblings().removeClass("inview");
     return true;
@@ -39,20 +36,16 @@ var locationPath = filterPath(location.pathname);
 $('a[href*="#"]').each(function () {
   var thisPath = filterPath(this.pathname) || locationPath;
   var hash = this.hash;
-  console.log(hash);
   if ($("#" + hash.replace(/#/, '')).length) {
     if (locationPath == thisPath && (location.hostname == this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
       var $target = $(hash), target = this.hash;
       if (target) {
         $(this).click(function (event) {
           event.preventDefault();
-          console.log(target);
-          console.log($("#work").scrollTop())
-          console.log( $target.offset().top);
           $('html,body,#content,#work').animate({scrollTop: $('#work').scrollTop() + $target.offset().top}, 1000, function () {
             location.hash = target;
             $target.focus();
-            if ($target.is(":focus")){ //checking if the target was focused
+            if ($target.is(":focus")){
               return false;
             }else{
               //$target.attr('tabindex','-1'); //Adding tabindex for elements not focusable
